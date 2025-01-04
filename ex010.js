6fd724cb1dd89f1cc3a6b189b05eb08922ba5f83
DonRibeiro <donavan.assis@gmail.com>
2025-01-04 10:59:07 -0300

    Update ex010.js

--- ex010.js
+++ ex010.js
@@ -1,6 +1,12 @@
-var agora = new Date()
-var hora = agora.getHours()
-console.log(`Voce tem idade ${idade}`)
-if (idade < 15) {
-    console.log('Não vota')
-} 
+let hora= 22
+console.log(`Agora são ${hora} horas`)
+if (hora < 12) {
+    console.log('Bom dia flor do dia')
+} else if(hora < 18){
+    console.log('Boa tarde no sol que arde')
+} else {
+    console.log('Boa noite oite oite')
+}
+
+
+
